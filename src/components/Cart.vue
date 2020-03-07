79ebb94e9fd3b9b58ea266a050ae9df114c330b6
basher43 <md.abulbasher43@gmail.com>
2020-03-08 03:25:31 +0600

    Remove from cart functionality added

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -2,7 +2,7 @@
     <div class="col-sm-3">
         <ul class="list-group">
             <li class="text-center bg-dark text-white font-weight-bold list-group-item">Cart</li>
-            <li class="list-group-item my-1" v-for="(data,i) in datas" :key="i">{{data.title}} <span class="float-right">{{data.price}}</span></li>
+            <li class="list-group-item my-1" v-for="(data,i) in datas" :key="i"> <button class="btn btn-danger mr-2" @click="removeItemFromCart(i)">x</button>{{data.title}} <span class="float-right">{{data.price}}</span></li>
 
             <li class="text-white list-group-item bg-dark">Total <span class="float-right">{{totalPrice}}</span></li>
         </ul>
@@ -22,6 +22,11 @@
                 return total
 
 
+            }
+        },
+        methods:{
+            removeItemFromCart(i){
+                this.datas.splice(i, 1)
             }
         }
     }
